e2735ae5fb4ca23e0c002fdfb61cb2b09647e4bb
govarthinisundararaj <35028923+govarthinisundararaj@users.noreply.github.com>
2022-05-25 11:42:49 +0530

    Update main.tf

--- Infrastructure/AWS_proj/main.tf
+++ Infrastructure/AWS_proj/main.tf
@@ -7,4 +7,12 @@ variable "region" {
     default = "us-east-1"
 }
 module "vpc" {
-    
+    source = "./modules"
+    region = var.region
+}
+
+resource "aws_instance" "my_instance" {
+    ami = module.ami_id
+    subnet_id = module.subnet_id
+    instance_type = "t2.micro"
+        }
